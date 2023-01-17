f8f42101f3757fd2f9d72a4452f3c0ac065aa141
guantou <mole30608@gmail.com>
2023-01-18 02:48:24 +0800

    Update findbook.py

--- findbook.py
+++ findbook.py
@@ -3,6 +3,9 @@ import re
 from bs4 import BeautifulSoup
 import time
 
+
+
+
 title = ''
 image = ''
 price = ''
@@ -12,30 +15,38 @@ publisher = ''
 isbn = ''
 
 base_api_link = "https://findbook.com.tw/"
-isbn = input("Enter ISBN: ").strip()
+isbn = '9789571077611'  # input("Enter ISBN: ").strip()
 print(isbn)
 with urllib.request.urlopen(base_api_link + isbn) as f:
+    time.sleep(1)
     text = f.read()
 soup = BeautifulSoup(text, 'html.parser')
-
-if soup==None:
+print(soup)
+if soup is None:
     print("Can't Searched")
 else:
     print('Searched')
-    time.sleep(1)
+    bookinfo = soup.find("ul", id="prodInfo2")
+    if bookinfo is None:
+        bookinfo = soup.find('table', class_='ISBNHead')
+    bookinfo=bookinfo.get_text()    
+
+
 
 title = soup.find('span', class_='b_name').text
 image = soup.find('img', class_='searchImg').get('src')
 price = max([int(p.find('div').text.strip('$').strip()) for p in soup.find_all('td', class_='IPrice')])
-bookinfo = soup.find("ul", id="prodInfo2").get_text()
 
-authors_regex = r"作者： (\S*)"
-publisher_regex = r"出版社： (\S*)"
+authors_regex = r"作者： ?(\S*)"
+publisher_regex = r"出版社： ?(\S*)"
 publishedDate_regex = r"出版日期：(\d{4}-\d{2}-\d{2})"
-
-authors = re.search(authors_regex, bookinfo) if authors is None else re.search(authors_regex, bookinfo).group(1)
-publisher = re.search(publisher_regex, bookinfo) if publisher is None else re.search(publisher_regex, bookinfo).group(1)
-publishedDate = re.search(publishedDate_regex, bookinfo) if publishedDate is None else re.search(publishedDate_regex, bookinfo).group(1)
+print(bookinfo)
+authors = "None" if re.search(
+    authors_regex, bookinfo) is None else re.search(authors_regex, bookinfo).group(1)
+publisher = "None" if re.search(publisher_regex, bookinfo) is None else re.search(
+    publisher_regex, bookinfo).group(1)
+publishedDate = "None" if re.search(publishedDate_regex, bookinfo) is None else re.search(
+    publishedDate_regex, bookinfo).group(1)
 
 print("\nTitle:", title)
 print("\nSummary:")
